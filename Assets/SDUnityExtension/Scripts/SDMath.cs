255d273f7ed79b6d21a13a756f237b8dac4548bd
sweets125 <an060875@naver.com>
2020-07-25 02:32:07 +0900

    - SDMath 클래스 추가

---
+++ Assets/SDUnityExtension/Scripts/SDMath.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class SDMath
+{
+    public static float Map(float val, float in_min, float in_max, float out_min, float out_max)
+    {
+        return (Mathf.Clamp(val, in_max, in_max) - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+    }
+
+
+}
