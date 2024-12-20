ce2b1f94917ead59134ce3bbf6851c11950e7ff9
sweetSD <anguone12@gmail.com>
2024-12-20 12:54:23 +0900

    프레임 GUI 스크립트 리팩토링

--- Assets/SDUnityExtension/Scripts/Debug/FPSCounter.cs
+++ Assets/SDUnityExtension/Scripts/Debug/FPSCounter.cs
@@ -1,22 +1,44 @@
 using UnityEngine;
-using UnityEngine.UI;
+using UnityEngine.Serialization;
 
 public class FPSCounter : MonoBehaviour
 {
-    [SerializeField] private Text text;
-    [SerializeField] private float refreshRate = 0.1f;
+    private static readonly Vector2 NativeScreenSize = new(640, 480);
+    
+    [SerializeField, Tooltip("Viewport 좌표계 기준 위치 좌표\nTop-Left: (0, 0), Bottom-Right: (1, 1)")] 
+    private Vector2 textPosition;
+    [SerializeField, Tooltip("Text 크기")] 
+    private int textSize = 10;
+    [SerializeField, Tooltip("Text 정렬")] 
+    private TextAnchor textAlignment = TextAnchor.UpperLeft;
+    
     private float timeCounter = 0f;
-
-    private void Start()
-    {
-        if (text == null) text = GetComponent<Text>();
-    }
+    private int tick = 0;
+    private int averageFps = 0;
 
     private void Update()
     {
-        if (timeCounter >= refreshRate)
-            text.text = $"{Mathf.Floor(1 / Time.deltaTime)} FPS";
+        timeCounter += Time.unscaledDeltaTime;
+        tick++;
+
+        if (timeCounter < 1) return;
         
-        timeCounter += Time.deltaTime;
+        timeCounter -= 1;
+        averageFps = tick;
+        tick = 0;
+    }
+
+    private void OnGUI()
+    {
+        var udt = Time.unscaledDeltaTime;
+        var text = $"Avg. {averageFps} ({1000f / averageFps}ms)\nCur. {Mathf.RoundToInt(1 / Time.unscaledDeltaTime)} ({udt * 1000}ms)";
+        var position = new Vector2(Screen.width * textPosition.x, Screen.height * textPosition.y);
+        var style = new GUIStyle()
+        {
+            fontSize = (int)(textSize * (Screen.width / NativeScreenSize.x)),
+            clipping = TextClipping.Overflow,
+            alignment = textAlignment
+        };
+        GUI.Label(new Rect(position, Vector2.zero), text, style);
     }
 }
