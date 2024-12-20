6cb7e7b050e9dc00e19a0b427325a7d6f57a548a
sweetSD <anguone12@gmail.com>
2024-12-20 17:53:49 +0900

    에디터 스크립트 리팩토링

--- Assets/SDUnityExtension/Scripts/Editor/SDEditorExtension.cs
+++ Assets/SDUnityExtension/Scripts/Editor/SDEditorExtension.cs
@@ -3,15 +3,9 @@ using UnityEngine;
 
 namespace SDUnityExtension.Scripts.Editor
 {
-    /// <summary>
-    /// Created by sweetSD. (Editor Extension)
-    /// 
-    /// 유니티 에디터 내부에서 사용할 수 있는 유틸리티 함수들이 있는 클래스입니다.
-    /// 
-    /// </summary>
     public class SDEditorExtension : MonoBehaviour
     {
-        [MenuItem("Tools/SDUnityExtension/Create Manager Prefab")]
+        [MenuItem("SDUnityExtension/Create Manager Prefab")]
         public static void CreateManagerPrefab()
         {
             var mngObject = Resources.Load<GameObject>("Prefab/SDManager");
---
+++ Assets/SDUnityExtension/Scripts/Editor/SDSceneUtilityWindow.cs
@@ -0,0 +1,47 @@
+﻿using System.IO;
+using UnityEditor;
+using UnityEditor.SceneManagement;
+using UnityEngine;
+
+namespace SDUnityExtension.Scripts.Editor
+{
+    public class SDSceneUtilityWindow : EditorWindow
+    {
+        private Vector2 scrollPos;
+        
+        [MenuItem("SDUnityExtension/Open Scene Utility Window")]
+        internal static void Init()
+        {
+            var window = (SDSceneUtilityWindow)GetWindow(typeof(SDSceneUtilityWindow), false, "SD Scene Utility");
+            window.position = new Rect(window.position.xMin + 100f, window.position.yMin + 100f, 200f, 400f);
+        }
+        
+        internal void OnGUI()
+        {
+            // Scenes View ===================================================================================
+            EditorGUILayout.BeginVertical();
+            scrollPos = EditorGUILayout.BeginScrollView(scrollPos, false, false);
+
+            GUILayout.Label("Scenes In Build", EditorStyles.boldLabel);
+            for (var i = 0; i < EditorBuildSettings.scenes.Length; i++)
+            {
+                var scene = EditorBuildSettings.scenes[i];
+                if (scene.enabled)
+                {
+                    var sceneName = Path.GetFileNameWithoutExtension(scene.path);
+                    var pressed = GUILayout.Button(i + ": " + sceneName, new GUIStyle(GUI.skin.GetStyle("Button")) { alignment = TextAnchor.MiddleLeft });
+                    if (pressed == false) continue;
+                    if (EditorSceneManager.SaveCurrentModifiedScenesIfUserWantsTo())
+                    {
+                        EditorSceneManager.OpenScene(scene.path);
+                    }
+                }
+            }
+            EditorGUILayout.EndScrollView();
+            EditorGUILayout.EndVertical();
+            // ===============================================================================================
+            
+            EditorGUILayout.Separator();
+        }
+    }
+}
